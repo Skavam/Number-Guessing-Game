fd1a1b0c144cc42f88ee0e02359d1ace847c7bea
Skavam <bastiencossette29@icloud.com>
2026-06-27 17:35:17 -0400

    Enhance number guessing game with color output
    Added color formatting to game messages and display attempts.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Number guessing game program by @Skavam on GitHub
 
 import random 
+from colorama import Fore
 
 def main():
     attempts = 0
@@ -8,25 +9,27 @@ def main():
     
     while True:
 
-        player_choice = int(input("Guess a number between 1 and 100: "))
+        player_choice = int(input(Fore.WHITE +"Guess a number between 1 and 100: "))
 
         if player_choice == number_to_guess:
-            print(f"Congratulations! You guessed the correct number. It was {number_to_guess}.")
+            print(Fore.GREEN + f"Congratulations! You guessed the correct number. It was {number_to_guess}.")
+            print(Fore.YELLOW + f"You guessed it in {attempts} attempts.")
+            print(Fore.WHITE + "Thanks for playing!")
             break
 
         elif player_choice < 1 or player_choice > 100:
-            print("Invalid input. Please enter a number between 1 and 100.")
+            print(Fore.RED + "Invalid input. Please enter a number between 1 and 100.")
 
         elif player_choice < number_to_guess:
-            print("Too low! Try again.")
+            print(Fore.LIGHTBLUE_EX + "Too low! Try again.")
             attempts += 1
 
         elif player_choice > number_to_guess:
-            print("Too high! Try again.")
+            print(Fore.LIGHTBLUE_EX + "Too high! Try again.")
             attempts += 1
 
         else:
-            print("Invalid input. Please enter a number between 1 and 100.")
+            print(Fore.RED + "Invalid input. Please enter a number between 1 and 100.")
 
 def generate_random_number():
     return random.randint(1, 100)
